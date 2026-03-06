d3a95ec2962a69b377ecebb4efef4b136351aad8
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2026-03-06 09:59:44 +0530

    feat: Add migration for fixing products v blocks

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -25,6 +25,7 @@ import * as migration_20260305_add_faq_block from './20260305_add_faq_block';
 import * as migration_20260305_add_product_blocks from './20260305_add_product_blocks';
 import * as migration_20260305_add_product_blocks_fk_columns from './20260305_add_product_blocks_fk_columns';
 import * as migration_20260305_consolidate_link_enums from './20260305_consolidate_link_enums';
+import * as migration_20260306_fix_products_v_blocks from './20260306_fix_products_v_blocks';
 
 export const migrations = [
   {
@@ -161,5 +162,10 @@ export const migrations = [
     up: migration_20260305_consolidate_link_enums.up,
     down: migration_20260305_consolidate_link_enums.down,
     name: '20260305_consolidate_link_enums'
+  },
+  {
+    up: migration_20260306_fix_products_v_blocks.up,
+    down: migration_20260306_fix_products_v_blocks.down,
+    name: '20260306_fix_products_v_blocks',
   },
 ];
